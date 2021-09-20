c4a6e0bd0f428bd6857d26299a2f6a1d520cb46a
Arslaan123Iqbal <58694423+Arslaan123Iqbal@users.noreply.github.com>
2021-09-20 15:56:58 +0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: 'flex', alignItems: 'center', color:"white" }}>
-          <DiCssdeck size="3rem" /> <span>Portfolio</span>
+          <DiCssdeck size="3rem" /> <span>Arslan Iqbal</span>
         </a>
       </Link>
     </Div1>
@@ -26,7 +26,7 @@ const Header = () =>  (
         </Link>
       </li>        
       <li>
-        <Link href="#about">
+        <Link href="#skills">
           <NavLink>Skills</NavLink>
         </Link>
       </li> 
